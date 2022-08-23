5ea6e208d2686fa5322d1f44cc2d4874ea39c252
Калюжный Андрей <87217824+zanzet@users.noreply.github.com>
2022-08-23 18:17:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('.skill').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__rollIn");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__fadeInDown");
+	        }
+	    });
+	});
+		$(window).scroll(function() {
+	    $('.form-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__bounceInUp");
+	        }
+	    });
+	});
+		$(window).scroll(function() {
+	    $('.mail').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__shakeX");
+	        }
+	    });
+	});	
+})
